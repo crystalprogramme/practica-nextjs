5d28775420524dadd3b1c9a7c46f653fcc2289c2
Ire <kleyrelda@gmail.com>
2025-04-06 18:48:50 -0500

    agrege mi proyecto

---
+++ app/ui/font.ts
@@ -0,0 +1,3 @@
+import {Roboto,Bebas_Neue} from 'next/font/google';
+export const roboto = Roboto({subsets:['latin'],weight:['400','500','700']});
+export const bebasNeue = Bebas_Neue({subsets:['latin'],weight:['400']});
